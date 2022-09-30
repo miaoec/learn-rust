dabb33b34aa505b3e2b4d965ae913428a131fa75
regmiao <regmiao@tencent.com>
2022-09-30 17:06:25 +0800

    feat:1.3

---
+++ 1.3/hello_cargo/target/debug/deps/hello_cargo-d4b125991477aa45.d
@@ -0,0 +1,5 @@
+/Users/regmiao/workspace/learn-rust/1.3/hello_cargo/target/debug/deps/hello_cargo-d4b125991477aa45: src/main.rs
+
+/Users/regmiao/workspace/learn-rust/1.3/hello_cargo/target/debug/deps/hello_cargo-d4b125991477aa45.d: src/main.rs
+
+src/main.rs:
---
+++ 1.3/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/regmiao/workspace/learn-rust/1.3/hello_cargo/target/debug/hello_cargo: /Users/regmiao/workspace/learn-rust/1.3/hello_cargo/src/main.rs
