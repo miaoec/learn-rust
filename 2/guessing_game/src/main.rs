2875dae63dc3036cc1ce690f5b37087da9aeaa51
regmiao <regmiao@tencent.com>
2022-10-14 15:49:07 +0800

    feat:loop

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -1,15 +1,30 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
 
     println!("Please input your guess");
 
+    let secret_number = rand::thread_rng().gen_range(1..190);
+
+    // println!("The secret number is: {secret_number}");
+
+ 
     let mut guess = String::new();
 
+    
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number)  {
+        std::cmp::Ordering::Less => println!("too small"),
+        std::cmp::Ordering::Equal => print!("you win"),
+        std::cmp::Ordering::Greater => println!("to0 big"),
+    }
+    // println!("You guessed: {guess}");   
 }
---
+++ 3/controlFlow/src/main.rs
@@ -0,0 +1,55 @@
+fn main_loop() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn main_while() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn main_for() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
+
+fn main_for_2() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+
+
+fn main(){
+    main_loop();
+    main_while();
+    main_for();
+    main_for_2();
+}
